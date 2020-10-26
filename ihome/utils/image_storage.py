a17cd4a0f7bc6329883f22aa7e2fa2168b28dc8c
pengyuer <592429355@qq.com>
2020-10-27 00:33:04 +0800

    宿舍版本“

---
+++ ihome/utils/image_storage.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from qiniu import Auth, put_data, etag, urlsafe_base64_encode
+import qiniu.config
+
+# 需要填写你的 Access Key 和 Secret Key
+access_key = 'Do9RJwd44_UnZ8eJYlSjquAuFtyi_uFnnGuIEAgz'
+secret_key = 'K3L78Rb5Y5g0TZ35gxqM2mX3Eo0CVYuh-s50vrd3'
+
+
+
+def storage(file_data):
+    """
+    上传文件到七牛
+    :param file_data: 要上传的文件数据
+    :return:
+    """
+    # 构建鉴权对象
+    q = Auth(access_key, secret_key)
+
+    # 要上传的空间
+    bucket_name = 'pythonmjc'
+
+    # 生成上传 Token，可以指定过期时间等
+    token = q.upload_token(bucket_name, None, 3600)
+
+    ret, info = put_data(token, None, file_data)
+
+    # print(info)
+    # print("*"*10)
+    # print(ret)
+    if info.status_code == 200:
+        # 表示上传成功, 返回文件名
+        return ret.get("key")
+    else:
+        # 上传失败
+        raise Exception("上传七牛失败")
+
+
+if __name__ == '__main__':
+    with open("./1.png", "rb") as f:
+        file_data = f.read()
+        storage(file_data)
